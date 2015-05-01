1aa974e423a53ab2b6612590e41745dd8cfc9686
charlyxie <c4xie@ucsd.edu>
2015-04-30 20:55:09 -0700

    added validations layer

---
+++ app/src/main/java/com/sos/saveourstudents/Validations.java
@@ -0,0 +1,22 @@
+package com.sos.saveourstudents;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by HTPC on 4/26/2015.
+ */
+public class Validations extends Activity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_signup);
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
