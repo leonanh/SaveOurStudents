72f5f0160d88741e7b21801ac090ad92dc85cfdd
yul379 <yul379@ucsd.edu>
2015-04-30 21:31:21 -0700

    Password&UserName check

--- app/src/main/java/com/sos/saveourstudents/Validations.java
+++ app/src/main/java/com/sos/saveourstudents/Validations.java
@@ -3,13 +3,19 @@ package com.sos.saveourstudents;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import java.util.regex.*;
+
+private final Pattern hasUppercase = Pattern.compile("[A-Z]");
+private final Pattern hasLowercase = Pattern.compile("[a-z]");
+private final Pattern hasNumber = Pattern.compile("\\d");
+private final Pattern hasSpecialChar = Pattern.compile("[^a-zA-Z0-9 ]");
 
 /**
  * Created by HTPC on 4/26/2015.
  */
 public class Validations extends Activity implements View.OnClickListener {
 
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +24,26 @@ public class Validations extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+    }
+    */
+
+    boolean = false;  //test result
+
+    public static boolean validUsername( String userName )
+    {
+        if (userName.length()  > 16)
+            return false;
+        if (userName.length() <  3)
+            return false;
+        return true;
+    }
+
+    public static boolean validPassword( String password )
+    {
+        if (password.length() < 8)
+            return false;
+
+        return true;
+
     }
 }
