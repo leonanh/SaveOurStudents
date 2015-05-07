1a9896add35bb6afe336f9f2151352b5b59879c8
Deagan Monahan <dsmonaha@ucsd.edu>
2015-05-07 10:13:06 -0700

    cleaned map fragment

--- app/src/main/java/com/sos/saveourstudents/FragmentMap.java
+++ app/src/main/java/com/sos/saveourstudents/FragmentMap.java
@@ -1,7 +1,9 @@
 package com.sos.saveourstudents;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,12 +14,14 @@ import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by deamon on 4/21/15.
@@ -37,7 +41,7 @@ public class FragmentMap extends Fragment implements
     private View rootView;
     private GoogleApiClient mGoogleApiClient;
 
-
+    private LayoutInflater minflater;
     public FragmentMap() {
         this.mContext = this.getActivity();
     }
@@ -45,18 +49,33 @@ public class FragmentMap extends Fragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        minflater = inflater;
         mContext = this.getActivity();
         rootView = inflater.inflate(R.layout.map_layout, container,
                 false);
 
 
+
+        createAndShowMap();
+
+
+        getMapData();
+
+
+
+        return rootView;
+    }
+
+
+
+    public void createAndShowMap(){
+
+        buildGoogleApiClient();
+
         mMapView = (MapView) rootView.findViewById(R.id.map);
         mMapView.onCreate(mBundle);
-
-
         mMap = mMapView.getMap();
-
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         try {
             MapsInitializer.initialize(mContext);
@@ -64,59 +83,80 @@ public class FragmentMap extends Fragment implements
             e.printStackTrace();
         }
 
-        setUpMapIfNeeded();
+        zoomToMyPosition();
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
 
-        //mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            // Use default InfoWindow frame
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            // Defines the contents of the InfoWindow
+            @Override
+            public View getInfoContents(Marker arg0) {
+
+                // Getting view from the layout file info_window_layout
+                View v = minflater.inflate(R.layout.info_window_layout, null);
+
+                // Getting the position from the marker
+                LatLng latLng = arg0.getPosition();
+
+                // Getting reference to the TextView to set latitude
+                TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+
+                // Getting reference to the TextView to set longitude
+                TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+
+                // Setting the latitude
+                tvLat.setText("Latitude:" + latLng.latitude);
+
+                // Setting the longitude
+                tvLng.setText("Longitude:" + latLng.longitude);
+
+                // Returning the view containing InfoWindow contents
+                return v;
+
+            }
+        });
+
+    }
+
+
+    public void getMapData(){
+
+
+        //TODO Make Volley call
+
+
+        //After volley create UI
+        createUI();
+
+
+    }
+
+    /**
+     * The final call after volley returns data,
+     * the volley call will save the data to a local datastructure (arraylist maybe)
+     * and build the map according to index of list
+     * TODO
+     */
+    private void createUI() {
+
         if(mMap != null)//Fernando test
         {
             Marker ucsd = mMap.addMarker(new MarkerOptions().position(UCSD)
                     .title("UCSD"));
             Marker geisel = mMap.addMarker(new MarkerOptions()
-                            .position(GEISEL)
-                            .title("GEISEL")
-                            .snippet("GEISEL is cool"));
+                    .position(GEISEL)
+                    .title("GEISEL")
+                    .snippet("GEISEL is cool"));
 
             mMap.setMyLocationEnabled(true);
         }
 
-
-        buildGoogleApiClient();
-
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLastLocation != null) {
-            TextView mLatitudeText = (TextView) rootView.findViewById(R.id.lat_text);
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            TextView mLongitudeText = (TextView) rootView.findViewById(R.id.long_text);
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-        }
-        //setUpMapIfNeeded();
-
-
-        //followMeLocationSource = new FollowMeLocationSource();
-
-
-        /*
-        if (mMap!=null){
-            Marker hamburg = mMap.addMarker(new MarkerOptions().position(UCSD)
-                    .title("UCSD"));
-            Marker kiel = mMap.addMarker(new MarkerOptions()
-                    .position(GEISEL)
-                    .title("GEISEL")
-                    .snippet("GEISEL is cool")
-                    .icon(BitmapDescriptorFactory
-                            .fromResource(R.drawable.ic_launcher)));
-
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSD, 15));
-
-            // Zoom in, animating the camera.
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-        }
-*/
-
-
-        return rootView;
     }
+
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(mContext)
@@ -125,44 +165,20 @@ public class FragmentMap extends Fragment implements
                 .addApi(LocationServices.API)
                 .build();
 
-        /*GoogleApiClient client = new GoogleApiClient.Builder(this)
-                .addApi(Plus.API)
-                .addScope(Plus.SCOPE_PLUS_LOGIN)
-                .setAccountName("users.account.name@gmail.com")
-                .build();*/
         mGoogleApiClient.connect();
     }
 
-
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            mMap = ((MapView) rootView.findViewById(R.id.map)).getMap();
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
 
     @Override
     public void onResume() {
         super.onResume();
         mMapView.onResume();
-        //mMap.setMyLocationEnabled(true);
-        //followMeLocationSource.getBestAvailableProvider();
 
-        // Get a reference to the map/GoogleMap object
-        //setUpMapIfNeeded();
     }
 
     @Override
     public void onPause() {
         mMapView.onPause();
-        //mMap.setMyLocationEnabled(false);
         super.onPause();
     }
 
@@ -188,22 +204,35 @@ public class FragmentMap extends Fragment implements
 
     }
 
-    // Marker info bubble
 
-    // Add window
-    /*static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-    Marker melbourne = mMap.addMarker(new MarkerOptions()
-            .position(MELBOURNE)
-            .title("Melbourne")
-            .snippet("Population: 4,137,400"));
 
-    // Show window
+    /**
+     * Magic
+     */
+    private void zoomToMyPosition(){
 
-    static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-    melbourne = mMap.addMarker(new MarkerOptions()
-            .position(MELBOURNE)
-            .title("Melbourne"));
-    melbourne.showInfoWindow();*/
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null)
+        {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        }
+
+
+
+    }
 
 
 }
