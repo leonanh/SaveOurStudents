e92b83e642e4267e4f956d7fda947f8503bc2921
Deagan Monahan <dsmonaha@ucsd.edu>
2015-05-14 16:44:24 -0700

    Changed create question layout to g+ look

--- app/src/main/java/com/sos/saveourstudents/FragmentCreateQuestion.java
+++ app/src/main/java/com/sos/saveourstudents/FragmentCreateQuestion.java
@@ -1,22 +1,20 @@
 package com.sos.saveourstudents;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.rey.material.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,7 +25,9 @@ public class FragmentCreateQuestion extends Fragment implements View.OnClickList
 
     private Context mContext;
 
-    TextView questionText;
+    private EditText questionEditText, topicEditText;
+
+    private ImageView sendButton, addTagsButton;
     LayoutInflater inflater;
     ViewGroup flowLayout;
     View rootView;
@@ -43,11 +43,16 @@ public class FragmentCreateQuestion extends Fragment implements View.OnClickList
         rootView = inflater.inflate(R.layout.fragment_create_question, container,
                 false);
 
-        flowLayout = (ViewGroup) rootView.findViewById(R.id.flow_container);
-        questionText = (TextView) rootView.findViewById(R.id.question_text);
-        questionText.setOnClickListener(this);
-        //Call this method to initiate volley request
-        getTagData();
+        sendButton = (ImageView) rootView.findViewById(R.id.send_button);
+        sendButton.setOnClickListener(this);
+
+        addTagsButton = (ImageView) rootView.findViewById(R.id.add_tag_button);
+        addTagsButton.setOnClickListener(this);
+
+        questionEditText = (EditText) rootView.findViewById(R.id.question_edit_text);
+        topicEditText = (EditText) rootView.findViewById(R.id.topic_edit_text);
+
+
 
 
         return rootView;
@@ -118,7 +123,7 @@ public class FragmentCreateQuestion extends Fragment implements View.OnClickList
 
                             System.out.println("popularTags "+popularTags.toString());
 
-                            showTags();
+                            //showTags();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -141,65 +146,6 @@ public class FragmentCreateQuestion extends Fragment implements View.OnClickList
         // Access the RequestQueue through your singleton class.
         Singleton.getInstance().addToRequestQueue(jsObjRequest);
 
-
-
-
-
-
-
-
-
-
-/*
-
-        JsonArrayRequest req = new JsonArrayRequest(url,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-
-                        try {
-
-                            JSONObject result = new JSONObject(response.toString());
-                            if(!result.getString("success").equalsIgnoreCase("1")){
-                                //Error getting data
-                                return;
-                            }
-                            if(result.getString("expectResults").equalsIgnoreCase("0")){
-                                //No results to show
-                                return;
-                            }
-                            else{
-                                //popularTags = result.getJSONArray("result");
-                            }
-
-
-                            System.out.println("popularTags "+popularTags.toString());
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-
-                        //showTags();
-
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //VolleyLog.d(TAG, "Error: " + error.getMessage());
-                System.out.println("error: "+error);
-            }
-        });
-
-
-
-        // Adding request to request queue
-        Singleton.getInstance().addToRequestQueue(req, tag_json_arry);
-
-*/
 
     }
 
@@ -229,37 +175,128 @@ public class FragmentCreateQuestion extends Fragment implements View.OnClickList
             System.out.println("v is selected: " + v.isSelected());
             v.setSelected(!v.isSelected());
 
-        } else if (v.getId() == R.id.question_text) {
+        }
+        else if(v == sendButton){
 
-            final com.rey.material.widget.EditText input = new com.rey.material.widget.EditText(mContext);
+            if (!topicEditText.getText().toString().equalsIgnoreCase("")) {
+                topicEditText.clearError();
 
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            lp.gravity = Gravity.TOP;
-            lp.setMargins(10,10,10,10);
-            input.setLayoutParams(lp);
-            //
-            //input.setLayoutParams(lp);
+                if (!questionEditText.getText().toString().equalsIgnoreCase("")) {
+                    questionEditText.clearError();
 
-            new AlertDialog.Builder(mContext)
-                    .setTitle("Post Question")
-                    .setMessage("")
-                    .setView(input)
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            Editable value = input.getText();
-                            System.out.println("wrote: "+value);
-                        }
-                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    // Do nothing.
+                    //TODO sendQuestionToServer();
+                } else {
+                    //Question edit text empty
+                    questionEditText.setError("Question is empty");
                 }
-            }).show();
+
+            } else {
+                //Topic empty
+                topicEditText.setError("Topic is empty");
+            }
+
+
+        }
+        else if(v == addTagsButton){
+
+            //TODO show tags dialog
 
         }
 
 
+    }
+
+
+
+    private void sendQuestionToServer(){
+
+        SharedPreferences sharedPref = mContext.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        String userId = sharedPref.getString("user_id", "");
+
+
+         /*
+
+         editor.putString("first_name", response.getJSONArray("result").getJSONObject(0).getString("first_name"));
+                        editor.putString("last_name", response.getJSONArray("result").getJSONObject(0).getString("last_name"));
+                        editor.putString("email", response.getJSONArray("result").getJSONObject(0).getString("email"));
+                        editor.putString("image", response.getJSONArray("result").getJSONObject(0).getString("image"));
+                        editor.putString("user_id", response.getJSONArray("result").getJSONObject(0).getString("user_id"));
+
+
+          */
+        System.out.println("Im'a send this question to server");
+
+        String url = "http://54.200.33.91:8080/com.mysql.services/rest/serviceclass/createQuestion?"+
+                "userId="+userId+
+                "&latitude="+
+                "&longitude="+
+                "&text="+
+                "&tags="+
+                "&tutor="+
+                "&studygroup=";
+
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                (JSONObject)null,
+                new Response.Listener<JSONObject>()
+                {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONObject result = new JSONObject(response.toString());
+                            if(!result.getString("success").equalsIgnoreCase("1")){
+                                //Error getting data
+                                return;
+                            }
+                            if(result.getString("expectResults").equalsIgnoreCase("0")){
+                                //No results to show
+                                return;
+                            }
+                            else{
+                                popularTags = result.getJSONArray("result");
+                            }
+
+
+                            System.out.println("popularTags "+popularTags.toString());
+
+                            //showTags();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                System.out.println("Error: " + error.toString());
+            }
+        });
+
+        // Access the RequestQueue through your singleton class.
+        Singleton.getInstance().addToRequestQueue(jsObjRequest);
+
+
+
+
+
+
+
 
     }
+
+
 
 
 }
