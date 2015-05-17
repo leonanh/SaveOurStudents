fc01e93d73d0eeb678ad246e745737f41a36ae90
Deagan Monahan <dsmonaha@ucsd.edu>
2015-05-16 17:14:34 -0700

    Member Join Added

--- app/src/main/java/com/sos/saveourstudents/supportclasses/GcmIntentService.java
+++ app/src/main/java/com/sos/saveourstudents/supportclasses/GcmIntentService.java
@@ -12,7 +12,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.sos.saveourstudents.MainActivity;
+import com.sos.saveourstudents.MemberJoinActivity;
 import com.sos.saveourstudents.R;
 
 /**
@@ -80,7 +80,7 @@ public class GcmIntentService extends IntentService {
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
+                new Intent(this, MemberJoinActivity.class), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
