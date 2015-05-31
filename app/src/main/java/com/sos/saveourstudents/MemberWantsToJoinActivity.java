f00a34b8f450f188d66e15ba3a5ef65fa264f39b
Deagan Monahan <dsmonaha@ucsd.edu>
2015-05-31 16:28:32 -0700

    Add user functionality

--- app/src/main/java/com/sos/saveourstudents/MemberWantsToJoinActivity.java
+++ app/src/main/java/com/sos/saveourstudents/MemberWantsToJoinActivity.java
@@ -1,18 +1,29 @@
 package com.sos.saveourstudents;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.rey.material.widget.FloatingActionButton;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
@@ -20,6 +31,9 @@ import com.rey.material.widget.FloatingActionButton;
  */
 public class MemberWantsToJoinActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private String mQuestionId;
+    private String mUserId;
+    private String mType;
     FloatingActionButton acceptButton;
     FloatingActionButton declineButton;
     RelativeLayout userProfile;
@@ -29,29 +43,20 @@ public class MemberWantsToJoinActivity extends AppCompatActivity implements View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_join_layout);
 
+        if(getIntent().getExtras() != null){
 
-        SharedPreferences sharedPref = getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+            System.out.println("Extras: "+getIntent().getExtras());
+            System.out.println("userId: "+getIntent().getExtras().getString("userId"));
+            System.out.println("type: "+getIntent().getExtras().getString("type"));
+
+            mUserId = getIntent().getExtras().getString("userId");
+            mQuestionId = getIntent().getExtras().getString("questionId");
+            mType = getIntent().getExtras().getString("type");
+        }
 
 
-        //String name = sharedPref.getString("first_name", "") + " "+ sharedPref.getString("last_name", "");
+        getUserInfo();
 
-        Student enteringStudent = new Student(sharedPref.getString("first_name", ""), sharedPref.getString("last_name", ""), 0, "UCSD", "Computer Engineering",
-                "Coffee Addict", null);
-
-
-
-        TextView joiningMemberName = (TextView) findViewById(R.id.member_name);
-        joiningMemberName.setText(enteringStudent.getFirstName());
-
-        TextView memberInfoName = (TextView) findViewById(R.id.joining_member_name);
-        memberInfoName.setText(enteringStudent.getFirstName() + " " + enteringStudent.getLastName());
-
-        TextView memberInfoMajor = (TextView) findViewById(R.id.joining_member_major);
-        memberInfoMajor.setText(enteringStudent.getMajor());
-
-        ImageView userImage = (ImageView) findViewById(R.id.joining_member_image);
-        getUserImage(sharedPref.getString("image", ""), userImage);
 
         userProfile = (RelativeLayout) findViewById(R.id.joining_member_info);
         acceptButton = (FloatingActionButton) findViewById(R.id.accept_fab);
@@ -62,37 +67,23 @@ public class MemberWantsToJoinActivity extends AppCompatActivity implements View
         declineButton.setOnClickListener(this);
     }
 
-    public void onClick(View v)
-    {
-        if(v == acceptButton)
-        {
-            //Accept member
-            Intent questionActivity = new Intent(this, ViewQuestionActivity.class);
-            questionActivity.putExtra("type", 0);
-            startActivity(questionActivity);
+    public void onClick(View v){
+
+        if(v == acceptButton){
+            addMembertoGroup();
+        }
+        else if (v == declineButton){
             finish();
         }
-        else if (v == declineButton)
-        {
-            //Decline member
-            //Intent questionActivity = new Intent(this, QuestionActivity.class);
-            //questionActivity.putExtra("type", 0);
-            //startActivity(questionActivity);
-            finish();
-        }
-        else if (v == userProfile)
-        {
-            //Check out member
+        else if (v == userProfile){
             startActivity(new Intent(this, ProfileActivity.class));
-            //finish();
         }
+
     }
 
 
 
     private void getUserImage(String imageUrl, final ImageView imageView){
-
-
 
         ImageLoader imageLoader = Singleton.getInstance().getImageLoader();
         // If you are using normal ImageView
@@ -112,9 +103,133 @@ public class MemberWantsToJoinActivity extends AppCompatActivity implements View
             }
         });
 
+    }
+
+
+    private void getUserInfo() {
+
+        List<NameValuePair> params = new LinkedList<NameValuePair>();
+        params.add(new BasicNameValuePair("userId", mUserId));
+
+        String paramString = URLEncodedUtils.format(params, "utf-8");
+        String url = "http://54.200.33.91:8080/com.mysql.services/rest/serviceclass/getUserById?"+paramString;
+
+
+        System.out.println("getUserById url: " + url);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url,
+                (JSONObject)null,
+                new Response.Listener<JSONObject>(){
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONObject result = new JSONObject(response.toString());
+                            System.out.println("getUserById result "+result);
+                            if(result.getString("success").equalsIgnoreCase("1")){
+
+                                JSONObject userObject = result.getJSONObject("result").getJSONArray("myArrayList").getJSONObject(0).getJSONObject("map");
+
+
+                                String name = userObject.getString("first_name")+ " "+userObject.getString("last_name");
+
+                                TextView joiningMemberName = (TextView) findViewById(R.id.member_name);
+                                joiningMemberName.setText(userObject.getString("first_name"));
+
+                                TextView memberInfoName = (TextView) findViewById(R.id.joining_member_name);
+                                memberInfoName.setText(name);
+
+                                TextView memberInfoMajor = (TextView) findViewById(R.id.joining_member_major);
+                                memberInfoMajor.setText(userObject.getString("major"));
+
+
+                                if(userObject.has("image")){
+                                    ImageView userImage = (ImageView) findViewById(R.id.joining_member_image);
+                                    getUserImage(userObject.getString("image"), userImage);
+                                }
+
+
+                            }
+                            else{
+                                //Error...
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error with connection or url: " + error.toString());
+            }
+
+        });
+
+        Singleton.getInstance().addToRequestQueue(jsObjRequest);
+
+    }
+
+    private void addMembertoGroup() {
+
+        List<NameValuePair> params = new LinkedList<NameValuePair>();
+        params.add(new BasicNameValuePair("questionId", mQuestionId));
+        params.add(new BasicNameValuePair("userId", mUserId));
+        params.add(new BasicNameValuePair("tutor", (mType.equalsIgnoreCase("2") ? 1 : 0)+"")); //((mType.equalsIgnoreCase("2") ? 1 : 0)+"")
+
+        String paramString = URLEncodedUtils.format(params, "utf-8");
+        String url = "http://54.200.33.91:8080/com.mysql.services/rest/serviceclass/acceptUser?"+paramString;
+
+
+        System.out.println("adduser url: " + url);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url,
+                (JSONObject)null,
+                new Response.Listener<JSONObject>(){
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONObject result = new JSONObject(response.toString());
+                            System.out.println("adduser result " + result);
+                            if(result.getString("success").equalsIgnoreCase("1")){
+
+                                //JSONObject userObject = result.getJSONObject("result").getJSONArray("myArrayList").getJSONObject(0).getJSONObject("map");
+
+                                finish();
+                            }
+                            else{
+                                //Error...
+                                Toast.makeText(MemberWantsToJoinActivity.this, "Error Accepting user", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error with connection or url: " + error.toString());
+            }
+
+        });
+
+
+        Singleton.getInstance().addToRequestQueue(jsObjRequest);
+
 
 
     }
+
+
 
 
 }
